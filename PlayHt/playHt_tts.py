bd7c64595343ebdeb8e8153425981a9f64defcd2
David Cardinal <github@cardinalphoto.com>
2024-12-28 15:08:22 -0800

    better error handling

--- PlayHt/playHt_tts.py
+++ PlayHt/playHt_tts.py
@@ -18,7 +18,122 @@ def create_directory(path):
     if not os.path.exists(path):
         os.makedirs(path)
     return path
+    # find the full path for an audio file to write
+    # we want to echo the repo & GCP heirarchy to save re-doing later
+    # e.g. <base>/task/language/shared/<item>.mp3
 
+def audio_file_path(task_name, item_name, audio_base_dir, lang_code):
+    full_file_folder = \
+        os.path.join(audio_base_dir, task_name, lang_code, "shared")
+    if not os.path.exists(full_file_folder):
+        os.makedirs(full_file_folder, exist_ok=True)
+    full_file_path = os.path.join(full_file_folder, item_name + ".mp3")
+    return full_file_path
+
+def processRow(index, ourRow, lang_code, voice, \
+               masterData, audio_base_dir, headers):
+
+    # reset error count for new row
+    errorCount = 0
+    retrySeconds = 2
+
+    # we should potentially filter these out when we generate diffs
+    if not (type(ourRow['labels']) == type('str')):
+        print(f"Item {ourRow['item_id']} doesn't have task assigned")
+        return 'Error'
+    data = {
+        # content needs to be an array, even if we only do one at a time
+        "content" : [ourRow[lang_code]],
+        "voice": voice,
+        "title": "Individual Audio",
+        "trimSilence": True
+    }
+
+    ## Use a While loop so we can retry odd failure cases
+    # see https://docs.play.ht/reference/api-convert-tts-standard-premium-voices
+    while True and errorCount < 5:
+
+        response = requests.post(API_URL, headers=headers, json=data) 
+
+        # 201 means that we got a response of some kind
+        if response.status_code == 201:
+            # results are packed into a json object
+            result = response.json()
+            logging.info(f"convert_tts: response for item={ourRow['item_id']}: transcriptionId={result['transcriptionId']}")
+        else:
+            logging.error(f"convert_tts: no response for item={ourRow['item_id']}: status code={response.status_code}")
+            # sometimes a retry works after no response
+            errorCount = errorCount+1
+            continue
+
+        # status is a little awkward to parse. Some errors aren't exactly errors
+        json_status = response.json()
+
+        if "transcriptionId" in json_status:
+            # This means that we've successfully started the transcription
+            transcription_id = json_status["transcriptionId"]
+            print(f"Conversion initiated. Transcription ID: {transcription_id}")
+        
+            # Poll the status until completion or we get 5 error returns
+            while True and errorCount < 5:
+                downloadURL = None # clear each time
+                status_params = {"transcriptionId": transcription_id}
+                status_response = requests.get(STATUS_URL, params=status_params, headers=headers)
+                status_data = status_response.json()
+
+                # Some errors are "fatal", some just mean a retry is needed
+                if 'error' in status_data:
+                    if status_data['error'] == True: # and \
+                        #status_data['message'] != 'Transcription still in progress':
+                        print(f'Error translating {ourRow['item_id']}')
+                        errorCount = errorCount+1
+                        ## Currently we only retry the STATUS call
+                        #  We should retry the entire call!
+                        continue # we want to start the loop over
+                        
+                if status_data["converted"] == True:
+                    print(f"Conversion for {ourRow['item_id']} completed successfully!")
+                    print(f"Audio URL: {status_data['audioUrl']}")
+                    # set the download URL for retrieval or get it right here?
+                    downloadURL = status_data['audioUrl']
+
+                    # At this point we should have an "audioURL" that we can retrieve
+                    # and then write out to the appropriate directory
+                    audioData = requests.get(downloadURL)
+
+                    # open file for writing
+                    # Download the MP3 file
+                    if audioData.status_code == 200 and ourRow['labels'] != float('nan'):
+                        with open(audio_file_path(ourRow["labels"], ourRow["item_id"], \
+                                lang_code, audio_base_dir), "wb") as file:
+                            file.write(audioData.content)
+                            # Write label ourRow in PD as translated?
+                            # write content to masterData
+                            
+                            # this doesn't work right!! Extra Column already added
+                            masterData[lang_code] = \
+                                np.where(masterData["item_id"] == ourRow["item_id"], \
+                                ourRow[lang_code], masterData[lang_code])
+
+                                # write as we go, so erroring out doesn't lose progress
+                                # Translated, so we can save it to a master sheet
+                            masterData.to_csv("translation_master.csv")
+                            errorCount = 0
+                        break
+                    else:
+                        print("Failed to download the MP3 file")
+                        break
+                else:
+                    # print(f"Conversion in progress. Status: {status_data['converted']}")
+                    # currently most tasks complet in about 1 second, so .5 seconds
+                    # seems like a good tradeoff between "over-polling" and "over-waiting"
+                    time.sleep(retrySeconds)  # Wait before checking again
+            else:
+                break
+    else:
+        # we've tried several times
+        return False
+    
 def main(
         input_file_path: str,
         master_file_path: str,
@@ -44,19 +159,7 @@ def main(
         item_id_column (str, optional): column name in the input file for stable and unique item ID. Defaults to 'item_id'.
         audio_dir (str, optional): The directory to store the audio files. Defaults to "audio_files/{lang_code}/".
     """
-    
-    # find the full path for an audio file to write
-    # we want to echo the repo & GCP heirarchy to save re-doing later
-    # e.g. <base>/task/language/shared/<item>.mp3
-    def audio_file_path(task_name, item_name):
-        full_file_folder = \
-            os.path.join(audio_base_dir, task_name,
-            lang_code, "shared")
-        if not os.path.exists(full_file_folder):
-            os.makedirs(full_file_folder, exist_ok=True)
-        full_file_path = os.path.join(full_file_folder, item_name + ".mp3")
-        return full_file_path
-    
+        
 
     if user_id is None:
         user_id = os.environ['PLAY_DOT_HT_USER_ID']
@@ -86,94 +189,9 @@ def main(
     
     for index, ourRow in inputData.iterrows():
 
-        # reset error count for new row
-        errorCount = 0
-
-        # we should potentially filter these out when we generate diffs
-        if not (type(ourRow['labels']) == type('str')):
-            print(f"Item {ourRow['item_id']} doesn't have task assigned")
-            continue
-        data = {
-            # content needs to be an array, even if we only do one at a time
-            "content" : [ourRow[lang_code]],
-            "voice": voice,
-            "title": "Individual Audio",
-            "trimSilence": True
-        }
-
-        ## Use a While loop so we can retry odd failure cases
-        # see https://docs.play.ht/reference/api-convert-tts-standard-premium-voices
-        while True and errorCount < 5:
-            response = requests.post(API_URL, headers=headers, json=data) 
-
-            if response.status_code == 201:
-                result = response.json()
-                logging.info(f"convert_tts: response for item={ourRow['item_id']}: transcriptionId={result['transcriptionId']}")
-            else:
-                logging.error(f"convert_tts: no response for item={ourRow['item_id']}: status code={response.status_code}")
-                errorCount = errorCount+1
-                continue
-
-            json_status = response.json()
-            if "transcriptionId" in json_status:
-                transcription_id = json_status["transcriptionId"]
-                print(f"Conversion initiated. Transcription ID: {transcription_id}")
-        
-                # Poll the status until completion
-                while True:
-                    if errorCount >= 5:
-                        break
-                    downloadURL = None # clear each time
-                    status_params = {"transcriptionId": transcription_id}
-                    status_response = requests.get(STATUS_URL, params=status_params, headers=headers)
-                    status_data = status_response.json()
-                    if 'error' in status_data:
-                        if status_data['error'] == True: # and \
-                            #status_data['message'] != 'Transcription still in progress':
-                            print(f'Error translating {ourRow['item_id']}')
-                            errorCount = errorCount+1
-                            break
-                        
-                    if status_data["converted"] == True:
-                        print(f"Conversion for {ourRow['item_id']} completed successfully!")
-                        print(f"Audio URL: {status_data['audioUrl']}")
-                        # set the download URL for retrieval or get it right here?
-                        downloadURL = status_data['audioUrl']
-
-                        # At this point we should have an "audioURL" that we can retrieve
-                        # and then write out to the appropriate directory
-                        audioData = requests.get(downloadURL)
-
-                        # open file for writing
-                        # Download the MP3 file
-                        if audioData.status_code == 200 and ourRow['labels'] != float('nan'):
-                            with open(audio_file_path(ourRow["labels"], ourRow["item_id"]), "wb") as file:
-                                file.write(audioData.content)
-                                # Write label ourRow in PD as translated?
-                                # write content to masterData
-                            
-                                # this doesn't work right!! Extra Column already added
-                                masterData[lang_code] = \
-                                    np.where(masterData["item_id"] == ourRow["item_id"], \
-                                          ourRow[lang_code], masterData[lang_code])
-
-                                # write as we go, so erroring out doesn't lose progress
-                                # Translated, so we can save it to a master sheet
-                                masterData.to_csv("translation_master.csv")
-                                errorCount = 0
-
-                        else:
-                            print("Failed to download the MP3 file")
-                        break
-                    else:
-                        # print(f"Conversion in progress. Status: {status_data['converted']}")
-                        # currently most tasks complet in about 1 second, so .5 seconds
-                        # seems like a good tradeoff between "over-polling" and "over-waiting"
-                        time.sleep(retry_seconds)  # Wait before checking again
-                else:
-                    break
-        else:
-            break
+        result = processRow(index, ourRow, lang_code=lang_code, voice=voice, \
+                            audio_base_dir=audio_base_dir, masterData=masterData, \
+                            headers=headers)
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
