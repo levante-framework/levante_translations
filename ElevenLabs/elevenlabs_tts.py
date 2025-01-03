4d6c4bb1cfd62b7a5b79f324bd16bccb73687b09
David Cardinal <github@cardinalphoto.com>
2025-01-03 13:34:52 -0800

    can generate audio from elevenlabs

--- ElevenLabs/elevenlabs_tts.py
+++ ElevenLabs/elevenlabs_tts.py
@@ -1,8 +1,9 @@
 # generate audio using ElevenLabs
 import os
+from elevenlabs import play
 from elevenlabs.client import ElevenLabs
 
-client = ElevenLabs(api_key="YOUR_API_KEY")
+client = ElevenLabs(api_key=os.getenv('ELEVEN_API_KEY'))
 
 
 # Generate audio from text
@@ -13,7 +14,7 @@ audio = client.generate(
 )
 
 # Play the audio (works locally)
-client.play(audio)
+play(audio)
 
 # Alternatively, save the audio to a file
 with open("output.mp3", "wb") as f:
