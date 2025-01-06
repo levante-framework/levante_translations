ac7fd2ab9304328fc5f9058c7f0d28e40fac979f
David Cardinal <github@cardinalphoto.com>
2025-01-06 08:59:30 -0800

    add dict for languages

---
+++ utilities/config.py
@@ -0,0 +1,12 @@
+# Re-factoring language settings out of code
+# Work in progress
+
+def get_languages(): 
+
+    language_dict = {\
+        'English': ['en', 'en-US-AriaNeural'],
+        'Spanish': ['es-CO', 'es-CO-SalomeNeural'],
+        'German': ['de', 'VickiNeural'],
+        }
+    
+    return language_dict       
