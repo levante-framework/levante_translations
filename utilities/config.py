ff82380b03cf012e158c5be0da2b7eb69d12d327
David Cardinal <github@cardinalphoto.com>
2025-01-06 10:27:57 -0800

    more language factoring

--- utilities/config.py
+++ utilities/config.py
@@ -1,6 +1,9 @@
 # Re-factoring language settings out of code
 # Work in progress
 
+# if we add it here can we use it throughout?
+language_list = {}
+
 def get_languages(): 
 
     language_list = {\
@@ -10,5 +13,10 @@ def get_languages():
         }
     
     # Later we can add HT voices and Eleven voices to each of these
-    
+    # e.g. language_list['English']['ht_voices' : ht_english_voice_list]
     return language_list       
+
+def add_voice_list(language, service, voice_list):
+    print('put add voice list here')
+
+    
