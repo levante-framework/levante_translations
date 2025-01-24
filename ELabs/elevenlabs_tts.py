e3a96b458ca0607cba1ea2742f9110e0b21c9189
David Cardinal <github@cardinalphoto.com>
2025-01-23 18:01:22 -0800

    better logging

--- ELabs/elevenlabs_tts.py
+++ ELabs/elevenlabs_tts.py
@@ -87,6 +87,7 @@ def processRow(index, ourRow, lang_code, voice, \
     try:
         audio = audio_client.generate(
             text=ourRow[lang_code],
+            voice=voice,
             model="eleven_multilingual_v2"
         )
 
@@ -94,7 +95,8 @@ def processRow(index, ourRow, lang_code, voice, \
         audio_filename = u.audio_file_path(ourRow["labels"], ourRow["item_id"], \
                 audio_base_dir, lang_code)
         save(audio, audio_filename)
-
+        print(f'Generated {audio_filename}')
+        
         # Update our "cache" of successful transcriptions                            
         masterData[lang_code] = \
             np.where(masterData["item_id"] == ourRow["item_id"], \
@@ -107,4 +109,5 @@ def processRow(index, ourRow, lang_code, voice, \
         return 'Success'    
 
     except:
+        print(f'Failed to generate {ourRow[lang_code]} for voice {voice}\n')
         return 'Failure'
