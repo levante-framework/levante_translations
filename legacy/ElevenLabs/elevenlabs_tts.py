89fac0cef013ae4f24f2e98129d0df11478a9d8c
David Cardinal <github@cardinalphoto.com>
2025-01-28 13:18:59 -0800

    remove references to output.mp3

--- legacy/ElevenLabs/elevenlabs_tts.py
+++ legacy/ElevenLabs/elevenlabs_tts.py
@@ -26,20 +26,6 @@ def list_voices(lang_code):
 def get_audio(text, voice):
     print("TBD")
 
-def example():
-    # Generate audio from text
-    audio = client.generate(
-        text="Hello, this is a test of the ElevenLabs API!",
-        voice="Rachel",
-        model="eleven_monolingual_v1"
-    )
-
-    # Play the audio (works locally)
-    play(audio)
-
-    # Alternatively, save the audio to a file
-    with open("output.mp3", "wb") as f:
-        f.write(audio)
 
 list_voices('en')
 
