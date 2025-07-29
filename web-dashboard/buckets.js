e69b59cbbae68dafbdbd36a9b2854912872182f9
David Cardinal <github@cardinalphoto.com>
2025-07-29 10:36:42 -0700

    bucket utilities

---
+++ web-dashboard/buckets.js
@@ -0,0 +1,32 @@
+export const TASK_BUCKET_NAMES_DEV = {
+    intro: 'levante-intro-dev',
+    vocab: 'levante-vocabulary-dev',
+    memorygame: 'levante-memory-dev',
+    roarinference: 'roar-inference',
+    adultreasoning: 'levante-math-dev',
+    heartsandflowers: 'levante-hearts-and-flowers-dev',
+    egmamath: 'levante-math-dev',
+    matrixreasoning: 'levante-pattern-matching-dev',
+    samedifferentselection: 'levante-same-different-dev',
+    trog: 'levante-sentence-understanding-dev',
+    mentalrotation: 'levante-shape-rotation-dev',
+    theoryofmind: 'levante-stories-dev',
+    shared: 'levante-tasks-shared-dev',
+  };
+  
+  export const TASK_BUCKET_NAMES_PROD = {
+    intro: 'levante-intro-prod',
+    vocab: 'levante-vocabulary-prod',
+    memorygame: 'levante-memory-prod',
+    roarinference: 'roar-inference',
+    adultreasoning: 'levante-math-prod',
+    heartsandflowers: 'levante-hearts-and-flowers-prod',
+    egmamath: 'levante-math-prod',
+    matrixreasoning: 'levante-pattern-matching-prod',
+    samedifferentselection: 'levante-same-different-prod',
+    trog: 'levante-sentence-understanding-prod',
+    mentalrotation: 'levante-shape-rotation-prod',
+    theoryofmind: 'levante-stories-prod',
+    shared: 'levante-tasks-shared-prod',
+  };
+  
