05d8c1c52578b91914b20e7dbcbbf7b131a942e4
digital-pro <david81@stanford.com>
2025-09-08 07:11:25 -0700

    update spanish

---
+++ activate_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to activate the virtual environment for levante_translations
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+echo "Virtual environment activated!"
+echo "You can now run Python scripts from this project."
+echo ""
+echo "To deactivate, simply type: deactivate"
+echo ""
